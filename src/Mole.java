74312f585e860d950f7404689530378538e01ade
David Persson <te16.david.persson@umea.nti.se>
2018-06-04 16:21:11 +0200

    Lade till ett par kommentarer

--- src/Mole.java
+++ src/Mole.java
@@ -1,22 +1,23 @@
 import java.awt.*;
 import java.util.Random;
 
+//ett försök att få figuren att ritas ut slumpmässigt på spelplanen.
 public class Mole implements Runnable{
     public static Rectangle moleModel;
     private static Color c3;
     int arrayX[] = new int[]{150, 275, 400}; //En array med X värdena från spel planen.
     int arrayY[] = new int[]{150, 275, 400};// En array med Y värdena från spel planen.
     public Mole(){
-        moleModel = new Rectangle(getRandomX(),getRandomY(),50,50);
+        moleModel = new Rectangle(getRandomX(), getRandomY(),50,50);
         c3 = new Color(0xCC87AA);
     }
 
-    public int getRandomX(){
+    public int getRandomX(){//Slumpmässar ett värde från arrayen med X-värden
         int rnd = (int)(Math.random()*arrayX.length);
         return arrayX[rnd];
     }
 
-    public int getRandomY(){
+    public int getRandomY(){//Slumpmässar ett värde från arrayen med Y-värden
         int rnd2 = (int)(Math.random()*arrayY.length);
         return arrayY[rnd2];
     }
@@ -42,3 +43,4 @@ public class Mole implements Runnable{
     }
 
 }
+
